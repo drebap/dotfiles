bce3d61494fa5e9f070cc64688192642134559ee
drebap <drebapt@gmail.com>
2017-08-22 02:21:46 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git clone https://github.com/errbap/vim 
+
+ln -sf ~/.config/dotfiles/vim ~/.vim
+ln -sf ~/.config/dotfiles/other/zshrc ~/.zshrc
+ln -sf ~/.config/dotfiles/other/tmux.conf ~/.tmux.conf
+ln -sf ~/.config/dotfiles/other/screenrc ~/.screenrc
+ln -sf ~/.config/dotfiles/other/clang-format ~/.clang-format
+ln -sf ~/.config/dotfiles/other/ideavimrc ~/.ideavimrc
