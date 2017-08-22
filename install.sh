47020818344976d1b3dc2e9c796f2039a6e0ae4d
drebap <drebapt@gmail.com>
2017-08-22 02:50:20 +0100

    Changed install script to use $PWD

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-git clone https://github.com/errbap/vim 
+if [ ! -d ./vim ]; then
+	git clone https://github.com/errbap/vim
+else
+	echo "Not cloning vim. Directory exists"
+fi
 
-ln -sf ~/.config/dotfiles/vim ~/.vim
-ln -sf ~/.config/dotfiles/other/zshrc ~/.zshrc
-ln -sf ~/.config/dotfiles/other/tmux.conf ~/.tmux.conf
-ln -sf ~/.config/dotfiles/other/screenrc ~/.screenrc
-ln -sf ~/.config/dotfiles/other/clang-format ~/.clang-format
-ln -sf ~/.config/dotfiles/other/ideavimrc ~/.ideavimrc
+ln -sf $PWD/vim ~/.vim
+ln -sf $PWD/other/zshrc ~/.zshrc
+ln -sf $PWD/other/tmux.conf ~/.tmux.conf
+ln -sf $PWD/other/screenrc ~/.screenrc
+ln -sf $PWD/other/clang-format ~/.clang-format
+ln -sf $PWD/other/ideavimrc ~/.ideavimrc
