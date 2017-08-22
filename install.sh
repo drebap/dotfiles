f11a4b29bc74ef4435a61f7fc5d6c8a949f51da7
drebap <drebapt@gmail.com>
2017-08-22 03:00:32 +0100

    Added flags to symlinks

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ else
 	echo "Not cloning vim. Directory exists"
 fi
 
-ln -sf $PWD/vim ~/.vim
-ln -sf $PWD/other/zshrc ~/.zshrc
-ln -sf $PWD/other/tmux.conf ~/.tmux.conf
-ln -sf $PWD/other/screenrc ~/.screenrc
-ln -sf $PWD/other/clang-format ~/.clang-format
-ln -sf $PWD/other/ideavimrc ~/.ideavimrc
+ln -sfn $PWD/vim/ ~/.vim
+ln -sf  $PWD/other/zshrc ~/.zshrc
+ln -sf  $PWD/other/tmux.conf ~/.tmux.conf
+ln -sf  $PWD/other/screenrc ~/.screenrc
+ln -sf  $PWD/other/clang-format ~/.clang-format
+ln -sf  $PWD/other/ideavimrc ~/.ideavimrc
