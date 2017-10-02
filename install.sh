4b2d6476935bbf0fdd3569a3701cd21a9c5f9e09
drebap <drebapt@gmail.com>
2017-10-02 11:56:49 +0100

    Added bash configuration

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ ln -sf  $PWD/other/tmux.conf ~/.tmux.conf
 ln -sf  $PWD/other/screenrc ~/.screenrc
 ln -sf  $PWD/other/clang-format ~/.clang-format
 ln -sf  $PWD/other/ideavimrc ~/.ideavimrc
+
+if [ `uname` == "Darwin" ]; then
+	ln -sf  $PWD/other/bash_profile ~/.bash_profile
+else
+	ln -sf  $PWD/other/bash_profile ~/.bashrc
+fi
